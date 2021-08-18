3511fae888fd6ab0072a2218abc4f998b3ca16ac
Arben Peposi <arben.peposi@outlook.fr>
2021-08-18 11:14:10 +0200

    afficher les chiffres dans le display et les reset

--- js/style.js
+++ js/style.js
@@ -1,16 +1,65 @@
-const display = document.querySelector('.screen-result');
-const form = document.getElementById("myForm");
-const number = document.querySelectorAll('form input[name="number"]');
-const reset = document.querySelector('form input[name="reset"]');
+/*----DOM-----*/
 
-for (elem of number) {
-    elem.addEventListener('click', function() {
-        display.textContent += this.value;
-    });
+const form = document.getElementById("myForm");
+const del = document.querySelector('form input[name="del"]');
+const reset = document.querySelector('form input[name="reset"]');
+const add = document.querySelector('form input[name="add"]');
+const soustract = document.querySelector('form input[name="soustract"]');
+const multiplicate = document.querySelector('form input[name="multiplicate"]');
+const division = document.querySelector('form input[name="division"]');
+const point = document.querySelector('form input[name="point"]');
+const numbers = document.querySelectorAll('form input[name="touch"]');
+let display = document.querySelector('.display');
+
+
+//Valeur par défaut du display 
+display.textContent = 0;
+
+//Stocker la valeur de l'écran précédent 
+let prev = 0;
+
+//Stocker l'affichage de l'écran 
+let show = '';
+
+//On stocke l'opération
+let operator = null;
+
+for (elem of numbers) {
+    elem.addEventListener('click', touchEvent);
+}
+
+/*Cette fonction réagit au clic sur les touches*/
+
+function touchEvent() {
+    console.log(this.value);
+    //display.textContent += this.value;
+    let touch = this.value;
+    //Si c'est un chiffre ou un point
+    if (touch || point) {
+        //Si l'affichage est vide, on récupère la touche , sinon on concatène avec ce qu'il y a déjà dans l'affichage
+        show = (show === "" ? touch.toString() : show + touch.toString());
+        display.textContent = show;
+    }
 }
 
 if (reset) {
-    reset.addEventListener('click', function() {
-        display.textContent = "";
-    })
+    reset.addEventListener('click', clear);
+}
+
+
+function clear() {
+    prev = 0;
+    show = "";
+    operator = null;
+    display.textContent = 0;
+}
+
+/*Effectue le calcul */
+function calcul(nb1, nb2, operator) {
+    //Convertir les chaines de caractères en nombres
+    if (operator === add) return nb1 + nb2;
+    if (operator === multiplicate) return nb1 * nb2;
+    if (operator === soustract) return nb1 - nb2;
+    if (operator === division) return nb1 / nb2;
+
 }
